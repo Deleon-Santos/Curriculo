d07422add25f982c2ddb9234bbd53e7dc61e17a2
DELEON SANTOS <145681679+Deleon-Santos@users.noreply.github.com>
2024-03-27 18:10:03 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,1 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
 
-        // Oculta todas as seções
-        document.querySelectorAll('section').forEach(section => {
-            section.classList.remove('ativa');
-        });
-
-        // Obtém o ID da seção correspondente ao link clicado
-        const targetId = this.getAttribute('href').substring(1);
-
-        // Ativa a seção correspondente
-        document.getElementById(targetId).classList.add('ativa');
-    });
-});
